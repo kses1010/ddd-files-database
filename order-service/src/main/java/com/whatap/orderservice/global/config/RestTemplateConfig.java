a97fc85403bfb15af959b19b692e167c7f23a583
Sunny <thstkd92@gmail.com>
2022-09-22 00:08:56 +0900

    feat : RestTemplate Config 추가

---
+++ order-service/src/main/java/com/whatap/orderservice/global/config/RestTemplateConfig.java
@@ -0,0 +1,38 @@
+package com.whatap.orderservice.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Configuration
+public class RestTemplateConfig {
+
+    private static final int CONNECT_TIMEOUT = 5000;
+    private static final int READ_TIMEOUT = 5000;
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setConnectTimeout(CONNECT_TIMEOUT);
+        httpRequestFactory.setReadTimeout(READ_TIMEOUT);
+
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(httpRequestFactory));
+        restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter());
+        return restTemplate;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+}
