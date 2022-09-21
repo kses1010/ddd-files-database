e54a31f2a14f3f6c8c883f422123853f1ad0617d
Sunny <thstkd92@gmail.com>
2022-09-21 21:34:22 +0900

    feat : Product 상세 조회 쿼리 추가

--- product-service/src/main/java/com/whatap/productservice/domain/product/ProductRepository.java
+++ product-service/src/main/java/com/whatap/productservice/domain/product/ProductRepository.java
@@ -1,6 +1,10 @@
 package com.whatap.productservice.domain.product;
 
+import java.util.Optional;
+
 public interface ProductRepository {
 
     Product save(Product product);
+
+    Optional<Product> findById(Long id);
 }
--- product-service/src/main/java/com/whatap/productservice/infra/product/ProductRepositoryImpl.java
+++ product-service/src/main/java/com/whatap/productservice/infra/product/ProductRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.whatap.productservice.infra.product;
 
 import com.whatap.productservice.domain.product.Product;
 import com.whatap.productservice.domain.product.ProductRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,10 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public Product save(Product product) {
         return productEntitySpringDataRepository.save(product.toEntity()).toDomain();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productEntitySpringDataRepository.findById(id).map(ProductEntity::toDomain);
     }
 }
