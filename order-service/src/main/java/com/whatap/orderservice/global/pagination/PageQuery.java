4618494697d4df77935cdf7e36665a6fc7142bb3
Sunny <thstkd92@gmail.com>
2022-09-22 00:58:15 +0900

    feat : page query util 추가

---
+++ order-service/src/main/java/com/whatap/orderservice/global/pagination/PageQuery.java
@@ -0,0 +1,24 @@
+package com.whatap.orderservice.global.pagination;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PageQuery {
+
+    private int page;
+    private int limit;
+
+    public PageQuery() {
+    }
+
+    public PageQuery(int page, int limit) {
+        this.page = page;
+        this.limit = limit;
+    }
+
+    public int offset() {
+        return (page - 1) * limit;
+    }
+}
---
+++ order-service/src/main/java/com/whatap/orderservice/global/pagination/PageQueryDto.java
@@ -0,0 +1,25 @@
+package com.whatap.orderservice.global.pagination;
+
+import javax.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@Setter
+public class PageQueryDto {
+
+    @Min(value = 1, message = "1보다 작은 페이지는 없습니다.")
+    private int page = 1;
+
+    @Range(min = 1, max = 255, message = "범위는 1에서 255까지만 제한됩니다.")
+    private int limit = 10;
+
+    public PageQueryDto() {
+    }
+
+    public PageQueryDto(int page, int limit) {
+        this.page = page;
+        this.limit = limit;
+    }
+}
