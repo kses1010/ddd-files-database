52f83b8719d6c57d4547b676cda9983c38d60224
Sunny <thstkd92@gmail.com>
2022-09-21 23:26:16 +0900

    feat : Product 삭제 서비스 구현

--- product-service/src/main/java/com/whatap/productservice/application/product/ProductDeleteAplService.java
+++ product-service/src/main/java/com/whatap/productservice/application/product/ProductDeleteAplService.java
@@ -1,7 +1,9 @@
 package com.whatap.productservice.application.product;
 
 import com.whatap.productservice.application.product.command.ProductDeleteCommand;
+import com.whatap.productservice.domain.product.Product;
 import com.whatap.productservice.domain.product.ProductRepository;
+import com.whatap.productservice.domain.product.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,7 @@ public class ProductDeleteAplService {
 
     @Transactional
     public void deleteProduct(ProductDeleteCommand command) {
-
+        Product product = productRepository.findById(command.getId()).orElseThrow(ProductNotFoundException::new);
+        productRepository.delete(product);
     }
 }
