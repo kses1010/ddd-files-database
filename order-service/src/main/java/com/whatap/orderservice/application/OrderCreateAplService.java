6e0d3c276447dd99c4f926bd0b14dd24e49a3b40
Sunny <thstkd92@gmail.com>
2022-09-22 00:37:01 +0900

    feat : Order 등록 서비스 구현

--- order-service/src/main/java/com/whatap/orderservice/application/OrderCreateAplService.java
+++ order-service/src/main/java/com/whatap/orderservice/application/OrderCreateAplService.java
@@ -1,15 +1,25 @@
 package com.whatap.orderservice.application;
 
 import com.whatap.orderservice.application.command.OrderCreateCommand;
-import com.whatap.orderservice.domain.Order;
+import com.whatap.orderservice.domain.order.Order;
+import com.whatap.orderservice.domain.order.OrderRepository;
+import com.whatap.orderservice.domain.order.model.ProductDetail;
+import com.whatap.orderservice.domain.order.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class OrderCreateAplService {
 
+    private final ProductService productService;
+    private final OrderRepository orderRepository;
+
     @Transactional
     public Order createOrder(OrderCreateCommand command) {
-
+        ProductDetail productDetail = productService.getProductDetail(command.getProductId());
+        Order order = Order.createOrder(productDetail.getId());
+        return orderRepository.save(order);
     }
 }
