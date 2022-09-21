d85f1bc4e30f4b78b84261b9f933e05c7f5fd9f8
Sunny <thstkd92@gmail.com>
2022-09-22 00:47:05 +0900

    feat : Order 상세 조회 쿼리 구현

--- order-service/src/main/java/com/whatap/orderservice/infra/order/OrderRepositoryImpl.java
+++ order-service/src/main/java/com/whatap/orderservice/infra/order/OrderRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.whatap.orderservice.infra.order;
 
 import com.whatap.orderservice.domain.order.Order;
 import com.whatap.orderservice.domain.order.OrderRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,10 @@ public class OrderRepositoryImpl implements OrderRepository {
     @Override
     public Order save(Order order) {
         return orderEntitySpringDataRepository.save(order.toEntity()).toDomain();
+    }
+
+    @Override
+    public Optional<Order> findById(Long id) {
+        return orderEntitySpringDataRepository.findById(id).map(OrderEntity::toDomain);
     }
 }
