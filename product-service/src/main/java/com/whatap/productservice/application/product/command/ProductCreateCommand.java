c8f97d755164b67622d5db9ef6b01fc34af0f2f3
Sunny <thstkd92@gmail.com>
2022-09-21 21:13:48 +0900

    refactor : command setter, 빈 생성자 삭제

--- product-service/src/main/java/com/whatap/productservice/application/product/command/ProductCreateCommand.java
+++ product-service/src/main/java/com/whatap/productservice/application/product/command/ProductCreateCommand.java
@@ -1,17 +1,12 @@
 package com.whatap.productservice.application.product.command;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ProductCreateCommand {
 
-    private String name;
-    private String description;
-
-    public ProductCreateCommand() {
-    }
+    private final String name;
+    private final String description;
 
     public ProductCreateCommand(String name, String description) {
         this.name = name;
