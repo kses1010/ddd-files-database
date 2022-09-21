8a954b2f85b88c92976a0cb5d2b80ab282ddcb89
Sunny <thstkd92@gmail.com>
2022-09-22 00:53:48 +0900

    feat : Order 전체 조회 서비스 구현

--- order-service/src/main/java/com/whatap/orderservice/application/OrderReadAplService.java
+++ order-service/src/main/java/com/whatap/orderservice/application/OrderReadAplService.java
@@ -4,6 +4,7 @@ import com.whatap.orderservice.application.query.OrderDetailQuery;
 import com.whatap.orderservice.domain.order.Order;
 import com.whatap.orderservice.domain.order.OrderRepository;
 import com.whatap.orderservice.domain.order.exception.OrderNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +18,9 @@ public class OrderReadAplService {
 
     public Order getOrderDetail(OrderDetailQuery query) {
         return orderRepository.findById(query.getId()).orElseThrow(OrderNotFoundException::new);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
     }
 }
