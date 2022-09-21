42555520d21087d1d26442057e9a30a9061aa876
Sunny <thstkd92@gmail.com>
2022-09-21 21:04:13 +0900

    feat : Product 등록 서비스 구현

---
+++ product-service/src/main/java/com/whatap/productservice/application/product/ProductCreateAplService.java
@@ -0,0 +1,21 @@
+package com.whatap.productservice.application.product;
+
+import com.whatap.productservice.application.product.command.ProductCreateCommand;
+import com.whatap.productservice.domain.product.Product;
+import com.whatap.productservice.domain.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ProductCreateAplService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public void createProduct(ProductCreateCommand command) {
+        Product product = Product.createProduct(command.getName(), command.getDescription());
+        productRepository.save(product);
+    }
+}
