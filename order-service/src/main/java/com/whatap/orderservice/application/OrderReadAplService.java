709d19baac655381412679ea47cc279c0c4cd59e
Sunny <thstkd92@gmail.com>
2022-09-22 00:49:03 +0900

    feat : Order 상세 조회 서비스 구현

--- order-service/src/main/java/com/whatap/orderservice/application/OrderReadAplService.java
+++ order-service/src/main/java/com/whatap/orderservice/application/OrderReadAplService.java
@@ -2,6 +2,8 @@ package com.whatap.orderservice.application;
 
 import com.whatap.orderservice.application.query.OrderDetailQuery;
 import com.whatap.orderservice.domain.order.Order;
+import com.whatap.orderservice.domain.order.OrderRepository;
+import com.whatap.orderservice.domain.order.exception.OrderNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class OrderReadAplService {
 
-    public Order getOrderDetail(OrderDetailQuery query) {
+    private final OrderRepository orderRepository;
 
+    public Order getOrderDetail(OrderDetailQuery query) {
+        return orderRepository.findById(query.getId()).orElseThrow(OrderNotFoundException::new);
     }
 }
