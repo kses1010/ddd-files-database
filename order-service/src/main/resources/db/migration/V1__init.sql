168a4f3f255520b8e65aec5e83b882e88cb37c70
Sunny <thstkd92@gmail.com>
2022-09-21 21:00:29 +0900

    feat : 수정 시간 컬럼 추가

--- order-service/src/main/resources/db/migration/V1__init.sql
+++ order-service/src/main/resources/db/migration/V1__init.sql
@@ -4,7 +4,8 @@ CREATE TABLE orders
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     product_id BIGINT    NULL COMMENT '상품 ID',
-    created_at TIMESTAMP NULL COMMENT '생성 시간'
+    created_at TIMESTAMP NULL COMMENT '생성 시간',
+    updated_at TIMESTAMP NULL COMMENT '수정 시간'
 );
 
 COMMIT;
--- product-service/src/main/resources/db/migration/V1__init.sql
+++ product-service/src/main/resources/db/migration/V1__init.sql
@@ -5,7 +5,8 @@ CREATE TABLE products
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(255) NULL COMMENT '상품 이름',
     description VARCHAR(511) NULL COMMENT '상품 상세',
-    created_at  TIMESTAMP    NULL COMMENT '생성 시간'
+    created_at  TIMESTAMP    NULL COMMENT '생성 시간',
+    updated_at  TIMESTAMP    NULL COMMENT '수정 시간'
 );
 
 COMMIT;
