ecc5cdf425c869d4cd2dde3be293c4b837586e68
Sunny <thstkd92@gmail.com>
2022-09-22 00:53:28 +0900

    feat : Order 전체 조회 쿼리 추가

--- order-service/src/main/java/com/whatap/orderservice/domain/order/OrderRepository.java
+++ order-service/src/main/java/com/whatap/orderservice/domain/order/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.whatap.orderservice.domain.order;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository {
@@ -7,4 +8,6 @@ public interface OrderRepository {
     Order save(Order order);
 
     Optional<Order> findById(Long id);
+
+    List<Order> findAll();
 }
--- order-service/src/main/java/com/whatap/orderservice/infra/order/OrderRepositoryImpl.java
+++ order-service/src/main/java/com/whatap/orderservice/infra/order/OrderRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.whatap.orderservice.infra.order;
 
 import com.whatap.orderservice.domain.order.Order;
 import com.whatap.orderservice.domain.order.OrderRepository;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -20,5 +22,12 @@ public class OrderRepositoryImpl implements OrderRepository {
     @Override
     public Optional<Order> findById(Long id) {
         return orderEntitySpringDataRepository.findById(id).map(OrderEntity::toDomain);
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return orderEntitySpringDataRepository.findAll().stream()
+            .map(OrderEntity::toDomain)
+            .collect(Collectors.toList());
     }
 }
