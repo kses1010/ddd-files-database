953e02458f06efc5cf5231d651cc7521f5c91acc
Sunny <thstkd92@gmail.com>
2022-09-22 14:04:36 +0900

    feat : HttpClient 의존성 추가 및 RestTemplateConfig 재설정

--- order-service/src/main/java/com/whatap/orderservice/global/config/RestTemplateConfig.java
+++ order-service/src/main/java/com/whatap/orderservice/global/config/RestTemplateConfig.java
@@ -2,10 +2,12 @@ package com.whatap.orderservice.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,15 +17,22 @@ public class RestTemplateConfig {
 
     private static final int CONNECT_TIMEOUT = 5000;
     private static final int READ_TIMEOUT = 5000;
+    private static final int MAX_CONN_TOTAL = 50;
+    private static final int MAX_CONN_PER_ROUTE = 20;
 
     private final ObjectMapper objectMapper;
 
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
         httpRequestFactory.setConnectTimeout(CONNECT_TIMEOUT);
         httpRequestFactory.setReadTimeout(READ_TIMEOUT);
+        HttpClient httpClient = HttpClientBuilder.create()
+            .setMaxConnTotal(MAX_CONN_TOTAL)
+            .setMaxConnPerRoute(MAX_CONN_PER_ROUTE)
+            .build();
 
+        httpRequestFactory.setHttpClient(httpClient);
         RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(httpRequestFactory));
         restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter());
         return restTemplate;
