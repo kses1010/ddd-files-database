3197da0c5e5484b4adf2eb88076fa7849e1efb9b
Sunny <thstkd92@gmail.com>
2022-09-22 13:43:33 +0900

    feat : Order 등록 validation 추가

--- order-service/src/main/java/com/whatap/orderservice/presentation/request/OrderCreateRequest.java
+++ order-service/src/main/java/com/whatap/orderservice/presentation/request/OrderCreateRequest.java
@@ -1,5 +1,6 @@
 package com.whatap.orderservice.presentation.request;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class OrderCreateRequest {
 
+    @NotNull
     private Long productId;
 
     public OrderCreateRequest() {
